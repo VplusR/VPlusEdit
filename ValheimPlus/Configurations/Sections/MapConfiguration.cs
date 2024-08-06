5df42cba866acb98967abacd959b61dbf6aa441c
VplusR <140290624+VplusR@users.noreply.github.com>
2024-08-05 22:48:23 -0400

    Update MapConfiguration.cs
    White space

--- ValheimPlus/Configurations/Sections/MapConfiguration.cs
+++ ValheimPlus/Configurations/Sections/MapConfiguration.cs
@@ -4,7 +4,7 @@
     {
         public bool shareMapProgression { get; internal set; } = false;
         public float exploreRadius { get; internal set; } = 50;
-        public bool preventPlayerFromTurningOffPublicPosition { get; internal set; } = false;        
+        public bool preventPlayerFromTurningOffPublicPosition { get; internal set; } = false;
         public bool shareAllPins { get; internal set; } = false;
         public bool displayCartsAndBoats { get; internal set; } = false;
     }
